f996bf702f371889e52a975724f41482e34cdf93
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-11-07 15:51:15 +0900

    Rspecの記述を追加

--- spec/system/crud_check.rb
+++ spec/system/crud_check.rb
@@ -16,7 +16,7 @@ RSpec.describe 'タスク管理機能', type: :system do
       before do
         visit new_task_path
         fill_in "title", with: 'manyou'
-        click_button 'Create Task'
+        click_on '登録する'
       end
       it '作成済みのタスクが表示される' do
         expect(page).to have_content 'manyou'
@@ -27,12 +27,39 @@ RSpec.describe 'タスク管理機能', type: :system do
       before do
         visit new_task_path
         fill_in "title", with: 'you'
-        click_button 'Create Task'
+        click_on '登録する'
         visit tasks_path
-        click_on 'Show'
+        click_on '詳細'
       end
       it '該当タスクの内容が表示されたページに遷移する' do
         expect(page).to have_content 'you'
+      end
+    end
+
+    context '複数件の登録のある一覧ページで' do
+      before do
+        Task.create!(id: '1',title: 't1')
+        Task.create!(id: '2',title: 't2')
+        Task.create!(id: '3',title: 't3')
+        visit tasks_path
+        click_on '詳細', match: :first
+      end
+      it '投稿が降順に並んでいるか確認' do
+        expect(page).to have_content 't3'
+      end
+    end
+
+    context '複数件の登録のある一覧ページで' do
+      before do
+        Task.create!(id: '1',title: 't1')
+        Task.create!(id: '2',title: 't2')
+        Task.create!(id: '3',title: 't3')
+        visit tasks_path
+        click_on 'Created at'
+        click_on '詳細', match: :first
+      end
+      it '投稿の並び替えが機能するか確認' do
+        expect(page).to have_content 't1'
       end
     end
   end
