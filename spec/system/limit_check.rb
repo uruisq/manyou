4bbdd528f574f72f71c2635f0f90a842bd5b01a0
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-11-19 11:00:20 +0900

    rspecの記述を修正

--- spec/system/limit_check.rb
+++ spec/system/limit_check.rb
@@ -5,9 +5,9 @@ RSpec.describe '終了期限のテスト', type: :system do
   describe '終了期限のテスト' do
     context '複数件の登録のある一覧ページで' do
       before do
-        Task.create!(id: '1',title: 'test1',limit: '2019/12/1 16:00')
-        Task.create!(id: '2',title: 'test2',limit: '2019/12/2 16:00')
-        Task.create!(id: '3',title: 'test3',limit: '2019/12/3 16:00')
+        Task.create!(id: '1',title: 'test1',limit: '2019/12/1 16:00',status: '未着手')
+        Task.create!(id: '2',title: 'test2',limit: '2019/12/2 16:00',status: '未着手')
+        Task.create!(id: '3',title: 'test3',limit: '2019/12/3 16:00',status: '未着手')
         visit tasks_path
         click_on '詳細', match: :first
       end
@@ -18,9 +18,9 @@ RSpec.describe '終了期限のテスト', type: :system do
 
     context '複数件の登録のある一覧ページで終了期限の並び替えボタンを押すと' do
       before do
-        Task.create!(id: '1',title: 'test1',limit: '2019/12/1 16:00')
-        Task.create!(id: '2',title: 'test2',limit: '2019/12/2 16:00')
-        Task.create!(id: '3',title: 'test3',limit: '2019/12/3 16:00')
+        Task.create!(id: '1',title: 'test1',limit: '2019/12/1 16:00',status: '未着手')
+        Task.create!(id: '2',title: 'test2',limit: '2019/12/2 16:00',status: '未着手')
+        Task.create!(id: '3',title: 'test3',limit: '2019/12/3 16:00',status: '未着手')
         visit tasks_path
         click_on 'Limit'
         click_on '詳細', match: :first
